229402bcbb38c25696339b1150486d90aa5bc7f3
Wylhem <wylhemdorv@gmail.com>
2022-03-11 12:22:47 +0100

    🚧 (graph): work on generateGrah function

--- src/graph/Maze.java
+++ src/graph/Maze.java
@@ -1,13 +1,10 @@
 package graph;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Maze {
 
@@ -21,19 +18,29 @@ public class Maze {
 
     private void generateMaze() {
         Path path = Paths.get(this.path);
-
+        BufferedReader reader;
+        List<char[]> maps = new ArrayList<char[]>();
         try {
-            String content = Files.readString(path);
-            System.out.println(content);
-            char[] chars = content.toCharArray();
-            for (int i = 0; i<chars.length;i++ ){
-                System.out.println(chars[i]);
+            reader = new BufferedReader(new FileReader(this.path));
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                maps.add(line.toCharArray());
+                line = reader.readLine();
             }
-        }catch (IOException e){
+            for (int i  = 0 ; i <maps.size();i++){
+                for (int j  = 0 ; j < maps.get(i).length;j++){
+                    System.out.print(maps.get(i)[j]);
+
+                }
+                System.out.println(" ");
+            }
+            reader.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-
-
 }
+
