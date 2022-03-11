245ea1563bed94b6ec17fb232b2b44ea21f9d960
Wylhem <wylhemdorv@gmail.com>
2022-03-11 11:57:06 +0100

    🚧 (main): working on graph

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,8 @@
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
+import graph.Maze;
 public class Main {
     public static void main(String[] args)  {
         System.out.println("Hello world");
-        // path = Paths.get("./Users/wylhemdorville/Desktop/apps/maze/src/ressources/oval_01.map.txt");
-
-        try {
-            File f = new File("./ressources/oval_01.map.txt");
-            Scanner sc = new Scanner(f);
-            while (sc.hasNextLine()) {
-                String line = sc.next();
-                System.out.println(line);
-            }
-            sc.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        Maze maze = new Maze("./ressources/oval_01.map.txt");
     }
 }
 
--- src/graph/Maze.java
+++ src/graph/Maze.java
@@ -1,15 +1,39 @@
 package graph;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Maze {
-    private char[][] maze;
 
-    Maze() {
+    private String path;
+    private String[][] map;
+
+    public Maze(String path) {
+        this.path = path;
+        generateMaze();
+    }
+
+    private void generateMaze() {
+        Path path = Paths.get(this.path);
+
+        try {
+            String content = Files.readString(path);
+            System.out.println(content);
+            char[] chars = content.toCharArray();
+            for (int i = 0; i<chars.length;i++ ){
+                System.out.println(chars[i]);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 
-    public static void generateMaze() throws IOException {
 
-    }
 }
