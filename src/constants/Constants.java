9c58a217c1dbc226e9436476aa98fb988c908f41
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-03-21 23:03:02 +0100

    Update Constants.java

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -3,6 +3,5 @@ package constants;
 public class Constants {
     public static final int DIAGONAL_COST = 14;
     public static final int HV_COST = 10;
-    //public static final String PATH ="/usr/src/mazeDorvilleElArabi/ressources";
-    public static final String PATH = "./ressources";
+    public static final String PATH ="/usr/src/mazeDorvilleElArabi/ressources";
 }
